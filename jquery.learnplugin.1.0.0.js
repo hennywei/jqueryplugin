8e9e186d4dbf6f6bd16e2f4f99675bc5aedcc6b1
hennywei <hennywei@gmail.com>
2015-02-02 15:28:34 +0800

    modify the class struction

--- jquery.learnplugin.1.0.0.js
+++ jquery.learnplugin.1.0.0.js
@@ -1,38 +1,55 @@
-/**
- *
- *
- */
-(function( $ ){
+;(function( $ ){
+
+    //function learnPlugin = 
+
+    var defaults = {
+        "errorMsg": "wrong email format",
+        "successMsg": "correct email"
+    };
+
+    var learnPlugin = function(element,options){
+        this.config = $.extend({},defaults,options);
+        this.element = element;
+
+        this.on = this.config.on;
+        this.success = this.config.success || (function(){});
+        this.fail = this.config.fail || (function(){});
+        //this.input = this;
+        this.init();
+    };
+
+    learnPlugin.prototype.init = function() {
+        var _ = this;    
+        if(typeof _.on === "string"){
+            _.element.bind(_.on,function(){
+                _.applyUserCall($(this));
+            });
+            _.element.each(function(){
+               _.applyUserCall($(this));
+            });
+        }
+        this.checkEmail(this.element); 
+    };
+    learnPlugin.prototype.applyUserCall = function ($field){
+        if (this.checkEmail($field)) {
+            this.success.call($field.get(0));
+            this.element.parent().find("span").text(this.config.successMsg);
+        } else {
+            this.fail.call($field.get(0));
+            this.element.parent().find("span").text(this.config.errorMsg);
+        }
+    };
+
+    learnPlugin.prototype.checkEmail = function($field){
+        if($field.is("input,text")){
+            var emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+            return emailRegExp.test($field.val());
+        }
+        return false;
+    };
+
     $.fn.learnPlugin = function(options) {
-        options = options || {};
-        var on = options.on;
-        var success = options.success || (function(){});
-        var fail = options.fail || (function(){});
-        var $input = this;
-
-        function checkEmail($field){
-            if($field.is("input,text")){
-                var emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-                return emailRegExp.test($field.val());
-            }
-            return false;
-        }
-
-        function applyUserCall($field) {
-            if (checkEmail($field)) {
-                success.call($field.get(0));
-            } else {
-                fail.call($field.get(0));
-            }
-        }
-        if(typeof on === "string"){
-            $input.bind(on,function(){
-                applyUserCall($(this));
-            });
-            $input.each(function(){
-               applyUserCall($(this));
-            });
-        }
-        return checkEmail($input);
-    }
-})( jQuery );
+        new learnPlugin(this.first(),options);
+        return this.first();
+    };
+}( jQuery ));
